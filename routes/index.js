6b12f2946dfebc6765b74d96995c5b4f9f74bfc5
Kazu Komoto <kazuhiro.komoto@students.makeschool.com>
2016-10-07 21:24:01 -0700

    modified

--- routes/index.js
+++ routes/index.js
@@ -1,62 +1,56 @@
 
 var express = require('express');
 var router = express.Router();
+var request = require('request')
 
 /* GET home page. */
-
-//verification code
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* verify webhook */
 router.get('/webhook', function(req, res) {
-
- if (req.query['hub.verify_token'] === 'token_test') {
-   res.send(req.query['hub.challenge']);
- } else {
-   res.send('Error, wrong validation token');
- }
+  if (req.query['hub.verify_token'] === 'token_test') {
+    res.send(req.query['hub.challenge']);
+  } else {
+    res.send('Error, wrong validation token');
+  }
 });
-
-//recievemessage
-router.post('/webhook/', function (req, res) {
- const events = req.body.entry[0].messaging;
- for (i = 0; i < events.length; i++) {
-   const event = req.body.entry[0].messaging[i];
-   if (event.message && event.message.text) {
-     const text = event.message.text;
-     console.log(text)
-   	 sendTextMessage(sender, "Text received, echo: " + text);
-   }
- }
-
- res.sendStatus(200);
-
-});
-
-//POST message
-var request = require('request')
 
 const ACCESS_TOKEN = process.env.FB_ACCESS_TOKEN
 
-
 function sendTextMessage(sender, text) {
- request({
-   url: 'https://graph.facebook.com/v2.6/me/messages',
-   qs: {access_token: ACCESS_TOKEN},
-   method: 'POST',
-   json: {
-     recipient: {id: sender},
-     message: {text: text}
-   }
-
- }, function(error, response, body) {
-   if (error) {
-     console.log('Error sending message: ', error);
-   } else if (response.body.error) {
-     console.log('Error: ', response.body.error);
-   }
- });
+  request({
+    url: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: {access_token: ACCESS_TOKEN},
+    method: 'POST',
+    json: {
+      recipient: {id: sender},
+      message: {text: text}
+    }
+  }, function(error, response, body) {
+    if (error) {
+      console.log('Error sending message: ', error);
+    } else if (response.body.error) {
+      console.log('Error: ', response.body.error);
+    }
+  });
 }
+
+/* webhook */
+router.post('/webhook', function (req, res) {
+  console.log('webhook!')
+  const events = req.body.entry[0].messaging;
+  for (i = 0; i < events.length; i++) {
+    const event = req.body.entry[0].messaging[i];
+    const sender = event.sender.id;
+    if (event.message && event.message.text) {
+      const text = event.message.text;
+      console.log(text)
+      sendTextMessage(sender, "Text received, echo: " + text);
+    }
+  }
+  res.sendStatus(200);
+});
 
 module.exports = router;
